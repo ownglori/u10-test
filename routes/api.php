ddcb2677e4ffca39b4a7cbbf112b39af70ad5436
Moraiene <vladislav.ponamaryov@gmail.com>
2024-03-27 00:31:13 +0200

    api.php

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('parcel')->group(function () {
+    Route::get('/create', [\App\Http\Controllers\ParcelController::class, 'create']);
+});
